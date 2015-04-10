0c6af9d775a888e39956a35d6cb43935f1160e47
Austin Pray <austin@austinpray.com>
2015-04-10 11:25:07 -0500

    Adds tests for version flag

---
+++ test/flags-version.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var chai            = require('chai');
+var assert          = chai.assert;
+var expect = require('chai').expect;
+
+var child = require('child_process');
+
+var cliVersion = require('../package.json').version;
+var abVersion = require('asset-builder/package.json').version;
+
+describe('flag: --version and -v', function() {
+  it(
+    'should print the version of CLI and local asset-builder for --version',
+    function(done) {
+      child.exec(
+        'node ' +
+        __dirname +
+        '/../bin/asset-builder-cli.js --version --cwd ./test',
+        function(err, stdout) {
+          expect(stdout).to.contain('cli version: ' + cliVersion);
+          expect(stdout).to.contain('local asset-builder: ' + abVersion);
+          done(err);
+        }
+      );
+    }
+  );
+  it(
+    'should print the version of CLI and local asset-builder for -v',
+    function(done) {
+      child.exec(
+        'node ' +
+        __dirname +
+        '/../bin/asset-builder-cli.js -v --cwd ./test',
+        function(err, stdout) {
+          expect(stdout).to.contain('cli version: ' + cliVersion);
+          expect(stdout).to.contain('local asset-builder: ' + abVersion);
+          done(err);
+        }
+      );
+    }
+  );
+});
